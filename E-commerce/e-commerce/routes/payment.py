a481cfddab6da128b9acfb81f5a757a1425b7ea3
Matheus38652 <160676725+Matheus38652@users.noreply.github.com>
2024-09-16 21:43:37 -0300

    Delete E-commerce directory

--- E-commerce/e-commerce/routes/payment.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, Blueprint
-
-pay_route = Blueprint("payment", __name__)
-
-@pay_route.route("/add")
-def add_met_pay():
-    return render_template('form_add_payment.html')
