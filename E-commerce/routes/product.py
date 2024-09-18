3a7a7e96155b4008ee8c112ba38bbf7adddb42d3
Matheus38652 <160676725+Matheus38652@users.noreply.github.com>
2024-09-17 21:16:44 -0300

    Delete E-commerce directory

--- E-commerce/routes/product.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, Blueprint
-
-product_route = Blueprint("product", __name__)
-
-@product_route.route("/add")
-def add_prod():
-    # adicionar produtos no site - post
-    return render_template('form_prod.html')
